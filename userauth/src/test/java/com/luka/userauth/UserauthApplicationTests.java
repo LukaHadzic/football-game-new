60d0cc3b3c0da27e818aea24aca713f5f159da88
ZERIXA3 <lukahadzic1@gmail.com>
2026-05-22 18:33:54 +0200

    Fixed sql line endings

--- userauth/src/test/java/com/luka/userauth/UserauthApplicationTests.java
+++ userauth/src/test/java/com/luka/userauth/UserauthApplicationTests.java
@@ -2,8 +2,10 @@ package com.luka.userauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserauthApplicationTests {
 
 	@Test
